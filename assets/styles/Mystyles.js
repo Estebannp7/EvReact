bf8bbaca7a7f92445617d6d91eadfc1992797e28
esteban palencia <eteban28@gmail.com>
2022-09-21 00:12:01 -0500

    evaluacion

---
+++ assets/styles/Mystyles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const styles1 = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  texts:{
+    fontSize:18,
+    fontWeight:'bold',
+    color:'red'
+  },
+  alignViews:{
+    alignItems: 'center',
+    
+    justifyContent: 'center',
+  }
+});
+
+const viewsChilds = StyleSheet.create({
+  views:{
+    width:'80%',
+    marginLeft:'5%',
+    marginTop:10,
+    borderRadius:10,
+  }
+})
+
+export {styles1, viewsChilds}
